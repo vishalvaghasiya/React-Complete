3730ff3c5b7869347fa01083431a5236dbfc4246
vishal vaghasiya <vishalvaghasiya.me@gmail.com>
2020-06-06 17:26:57 +0530

    my component develop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import Vish from "./component/vish";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Vish <code>Hi How are you?</code> -.-</p>
-        <br/>
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Vish <code>Hi How are you?</code> -.-</p>
+                <br/>
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+            <Vish />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/vish.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Vish extends React.Component {
+    render() {
+        return (
+            <div className='vish'>
+                <h1>Name : Vishal </h1>
+                <p>age: 23</p>
+                <h4>hobby: coding</h4>
+            </div>
+        );
+    }
+}
+
+export default Vish;
